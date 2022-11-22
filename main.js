ff77b7cdc6ad0acd92e1eab0ef472ebd57fefdad
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-11-22 17:50:09 +0000

    Broken already

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const Starling = require('starling-developer-sdk')
+import conf from './config.json'
+
+const client = new Starling({
+    apiUrl: 'https://api-sandbox.starlingbank.com',
+    accessToken: conf.key
+})
+
+client.account.getAccounts()
+    .then(({ data }) => console.log(data))
+    .catch(err => console.log(err))
